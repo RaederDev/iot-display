88178c5b3a3b5efda670955d298019e21bfab958
N0ps32 <github@raeder.technology>
2022-06-03 00:16:31 +0200

    fix setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -22,8 +22,8 @@ fi
 mkdir /opt/iot-display
 chown display:display /opt/iot-display
 sudo -u display git clone https://github.com/RaederDev/iot-display.git /opt/iot-display
-sudo -u display sh -c "cd /opt/iot-display && /opt/iot-display/bin/update-server.sh"
 sudo -u display cp /opt/iot-display/config/config.tpl.json /opt/iot-display/config/config.json
+sudo -u display sh -c "cd /opt/iot-display && /opt/iot-display/bin/update-server.sh"
 ln -s /opt/iot-display/bin/system/iot-display.service /lib/systemd/system/iot-display.service
 
 systemctl daemon-reload
